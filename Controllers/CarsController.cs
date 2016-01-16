70490da79dfdefd7e8568e8126c6cc63903c755c
Tim Peara <tpeara@gmail.com>
2016-01-16 17:45:31 -0500

    2nd Commit
    Further work on jscript to integrate browser action with data base ajax
    calls.

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -9,7 +9,24 @@ namespace ProductsApp.Controllers
     public class CarsController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
+        /// <summary>
+        /// This API returns a list of cars records for based upon 4 parameters 
+        /// and a sort code.  All parameters and the sort code are optional, but parameter names 
+        /// must be provided in the GET request.
+        /// For example, the most basic call is: url/api/cars/full?year=&make=&model=&trim=&sort=
+        /// where url = http:\\tpeara.carfinder.com (?)
+        /// Otherwise, year is a 4 digit numbrer, e.g.,  2011
+        /// make is the manufacturer's name, e.g., honda
+        /// model is the type of car, e.g., accord
+        /// trim is its configuration, e.g., EX-L 2dr Coupe (2.4L 4cyl CVT)
+        /// sort, default by date, 1 by make, 2 by model
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="make"></param>
+        /// <param name="model"></param>
+        /// <param name="trim"></param>
+        /// <param name="sort"></param>
+        /// <returns></returns>
         [Route("Full")]
         public IHttpActionResult GetCars4YrMakeModelTrim(string year, string make, string model, string trim, string sort)
         {
@@ -25,7 +42,12 @@ namespace ProductsApp.Controllers
             return Ok(retval);
 
         }
-
+        /// <summary>
+        /// This API returns a list of the distinct car makes for the year in question
+        /// For example, for 2001 it returns ... bugatti, buick, cadilac, chevrolet...
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
         [Route("Makes")]
         public IHttpActionResult GetCarMakes4Yr(string year)
         {
--- Scripts/CarFinder.js
+++ Scripts/CarFinder.js
@@ -1,46 +1,223 @@
 ﻿$(document).ready(function () {
-    var http = 'http://www.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/2006/make/acura/model/MDX?format=json';
-        
-    $.ajax({
-        url: http,
-        dataType: 'jsonp',
-        jsonpCallback: "callback",
-        type: "GET",
-        success: function (data) {
-            var div = $('#data');
-            for (var i = 0; i < data.Results.length; i++) {
-                $(div).append('<div>' + data.Results[i].Component + " "+
-                    +data.Results[i].ModelYear + " " +
-                    + data.Results[i].Conequence + '</div>');
-            }
-        }
-    }).done(function (data) {
-       // if (console && console.log) {
-        console.log("Sample of data:"+ data); //.slice(0, 100));
-        
-       // }
+
+    initializeErrors();
+
+    yearSlider(1936, 2015, 1988);  // replace w/ API call for min & max
+
+    $('#yearSlider').on('mouseleave', function () {
+
+        getMakes($('#year').val());
+
     });
 
-    initializeYearSlider(1936, 2015, 1988);  // replace w/ API call for min & max
+    $('#makeSlider').on('mouseleave', function () {
 
+        getModels($('#year').val(),$('#model').val());
+
+    });
 });
 
-     function initializeYearSlider(yr_first,yr_last,yr_start) {
+
+function modelSlider(xml) {
+    data = xml;
+    console.log('data in modelSlider:' + data + '\n typeof' + typeof data + 'length ' + data.length);
+    var i = 0;
+    var modelsArray = [];
+    $(data).find('model_name').each(function () {
+        modelsArray[i] = $(this).text();
+        console.log('modelsArray = ' + modelsArray[i]);
+        i++;
+    });
+    var j = modelsArray.length;
+    
+    $('#model').val(modelsArray[0]);
+    $('#model').css('visibility', 'hidden');
+    $('#modelSlider').slider({
+        // orientation: 'vertical',
+        range: 'min',
+        min: 0,
+        max: j,
+        value: Math.round(j / 2, 0),
+        slide: function (event, ui) {
+            $('#model').val(modelsArray[ui.value]);
+            $('#model').css('visibility', 'visible');
+        },
+        stop: function (event, ui) {
+            // what to do after exiting slider
+        }
+    });
+    $('#model').val($('#modelSlider').slider('value'));
+};
+
+function getModels(year,make) {
+        
+    var _year = year || '';
+    var _make = make || '';
+    var http = 'http://localhost:57795/api/cars/full?year=' + _year +
+        '&make=' + _make + '&model=&trim=&sort=2';
+    
+    console.log('getModels search string>>>' + http + '<<<');
+    var modelsArray = [];
+
+    $.ajax({
+        url: http,
+        dataType: 'xml',
+        error: function(xhr, error){
+            console.debug(xhr); console.debug(error);
+            modelsArray = NULL;
+        },
+        
+        type: 'GET',
+        success: function (data) {
+            var div = $('#models');
+            console.log('getModels data in success:' + data + '\n typeof' + typeof data + 'length ' + data.length);
+            var i = 0;
+            $(data).find('model_name').each(function() {
+                modelsArray[i] = $(this).text();
+                console.log('modelsArray = ' + modelsArray[i]);
+                i++;
+            });
+            modelSlider(data);
+
+        }
+    }).done(function (data) {
+        // var x = data.getElementsByTagName('string');
+        console.log('Exiting get models: length of makesArr: ' + modelsArray.length + ' typeof: ' + typeof modelsArray);
+    });
+
+}
 
 
-        $("#slider-vertical").slider({
-            orientation: "vertical",
-            range: "min",
-            min: yr_first,
-            max: yr_last,
-            value: yr_start,
-            slide: function (event, ui) {
-                $("#amount").val(ui.value);  
-            },
-            stop: function( event,ui) {
-                alert("finished sliding!!!");
-            }
-        });
-        $("#amount").val($("#slider-vertical").slider("value"));
+function yearSlider(yr_first, yr_last, yr_start) {
+    console.log('Inside year Slider');
+    $('#yearSlider').slider({
+        // orientation: 'vertical',
+        range: 'min',
+        min: yr_first,
+        max: yr_last,
+        value: yr_start,
+        slide: function (event, ui) {
+            $('#year').val(ui.value);  
+        },
+        stop: function( event,ui) {
+            // what to do after exiting slider
+        }
+    });
+    $('#year').val($('#yearSlider').slider('value'));
+};
 
-    };
+function makeSlider(xml) {
+    data = xml;
+    var i = 0;
+    var makesArray = [];
+    $(data).find('string').each(function () {
+        makesArray[i] = $(this).text();
+        // console.log('makesArray = ' + makesArray[i]);
+        i++;
+    });
+    var j = makesArray.length;
+    
+    $('#make').val(makesArray[0]);
+    $('#make').css('visibility', 'hidden');
+    $('#makeSlider').slider({
+        // orientation: 'vertical',
+        range: 'min',
+        min: 0,
+        max: j,
+        value: Math.round(j/2,0),
+        slide: function (event, ui) {
+            $('#make').val(makesArray[ui.value]);
+            $('#make').css('visibility', 'visible');
+        },
+        stop: function (event, ui) {
+            // what to do after exiting slider
+        }
+    });
+    $('#make').val($('#makeSlider').slider('value'));
+};
+
+     function getMakes(year) {
+        
+         var _year = year || '';
+         var http = 'http://localhost:57795/api/cars/makes?year=' + _year;
+         // console.log('search string>>>' + http + '<<<');
+         var makesArray = [];
+
+         $.ajax({
+             url: http,
+             dataType: 'xml',
+             error: function(xhr, error){
+                 console.debug(xhr); console.debug(error);
+                 makesArray = NULL;
+             },
+             // jsonpCallback: 'callback',
+             type: 'GET',
+             success: function (data) {
+                 var div = $('#makes');
+                     var i = 0;
+                 $(data).find('string').each(function() {
+                     makesArray[i] = $(this).text();
+                     // console.log('makesArray = ' + makesArray[i]);
+                     i++;
+                 });
+                 makeSlider(data);
+
+             }
+         }).done(function (data) {
+//             var x = data.getElementsByTagName('string');
+             console.log('Exiting get makes: length of makesArr: ' + makesArray.length + ' typeof: ' + typeof makesArray);
+         });
+
+     }
+
+
+
+     //$(function initializeErrors() {
+     function initializeErrors() {
+         console.log('Inside initialize Errors');
+         // alert('Inside ajax setup');
+         $.ajaxSetup({
+             error: function (jqXHR, exception) {
+                 if (jqXHR.status === 0) {
+                     alert('Not connect.\n Verify Network.');
+                 } else if (jqXHR.status == 404) {
+                     alert('Requested page not found. [404]');
+                 } else if (jqXHR.status == 500) {
+                     alert('Internal Server Error [500].');
+                 } else if (exception === 'parsererror') {
+                     alert('Requested JSON parse failed.');
+                 } else if (exception === 'timeout') {
+                     alert('Time out error.');
+                 } else if (exception === 'abort') {
+                     alert('Ajax request aborted.');
+                 } else {
+                     alert('Uncaught Error.\n' + jqXHR.responseText);
+                 }
+             }
+         });
+     };
+
+     
+     function getRecalls(year, make, model) {
+         var http = 'http://www.nhtsa.gov/webapi/api/Recalls/vehicle/year/2006/make/acura/model/MDX?format=json';
+
+         $.ajax({
+             url: http,
+             dataType: 'jsonp',
+             jsonpCallback: 'callback',
+             type: 'GET',
+             success: function (data) {
+                 var div = $('#data');
+                 for (var i = 0; i < data.Results.length; i++) {
+                     $(div).append('<div>' + data.Results[i].Component + ' ' +
+                         +data.Results[i].year + ' ' +
+                         +data.Results[i].Conequence + '</div>');
+                 }
+             }
+         }).done(function (data) {
+             // if (console && console.log) {
+             console.log('Sample of data:' + data); //.slice(0, 100));
+
+             // }
+         });
+     }
